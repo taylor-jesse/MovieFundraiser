bb9824c411d35995fb152d20cbb066e884debd2b
taylor.jesse <taylor.jesse@awatapu.school.nz>
2021-07-30 11:54:35 +1200

    Int checker

---
+++ int_checker.py
@@ -0,0 +1,12 @@
+def int_check(question):
+
+    error = "please enter whole number that is more than 12 and less than 130"
+
+    valid = False
+    while not valid:
+
+            #ask user for a number and check it is valid
+        try:
+            response = int(input(question))
+
+            if response <= 0:
