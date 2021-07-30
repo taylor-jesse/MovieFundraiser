3cbe60f0794cba2dc46e7b353a869e72c788b122
taylor.jesse <taylor.jesse@awatapu.school.nz>
2021-07-30 12:12:04 +1200

    base code

--- base_component.py
+++ base_component.py
@@ -1,3 +1,6 @@
+
+# ***** Main routine ****
+#set up directories
 # not blank function
 def not_blank(question):
     valid = False
@@ -9,21 +12,32 @@ def not_blank(question):
 
         else:
             print("This can't be blank please enter a name")
+#Int checker
+name = ""
+count = 0
+max_tickets = 5
+while name != "xxx" and count < max_tickets:
+    print("You have {} seats "
+          "left".format(max_tickets - count))
 
-# ***** Main routine ****
-name = not_blank("What is your name? ")
-age = not_blank("How old are you? ")
+    # Get details
+    name = input ("Name: ")
+    count += 1
+    print ()
 
-
-
-#set up directories
+    if count == max_tickets:
+        print("We have sold out of tickets!")
+else:
+    print("you have sold {} tickets.    \n")
+    "there are {} places still availble" .format(count, max_tickets - count)
 
 #ask the user if they have used the program before or show
 
 #loop into net ticket details
     #Get name
-
+name = not_blank("What is your name? ")
     #get age
+age = not_blank("How old are you? ")
 
     #calculate ticket price
 
