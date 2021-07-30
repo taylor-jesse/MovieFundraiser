6e40dbbbd35b7fdc46c071605f0afcf093e1a392
taylor.jesse <taylor.jesse@awatapu.school.nz>
2021-07-30 12:12:17 +1200

    ticket loop

--- ticket loop.py
+++ ticket loop.py
@@ -4,7 +4,18 @@ name = ""
 count = 0
 max_tickets = 5
 
-while name != "xxx" and count <+ max_tickets:
-    #get details
-    name = input("what is your name?")
+while name != "xxx" and count < max_tickets:
+    print("You have {} seats "
+          "left".format(max_tickets - count))
+
+    # Get details
+    name = input ("Name: ")
     count += 1
+    print ()
+
+    if count == max_tickets:
+        print("We have sold out of tickets!")
+else:
+    print("you have sold {} tickets.    \n")
+    "there are {} places still availble" .format(count, max_tickets - count)
+
