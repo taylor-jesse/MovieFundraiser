4744c125dd391187d345d4fb878a5c7f4b6d2eda
taylor.jesse <taylor.jesse@awatapu.school.nz>
2021-08-05 14:37:56 +1200

    Base code

--- base_component.py
+++ base_component.py
@@ -1,6 +1,5 @@
 
-# ***** Main routine ****
-#set up directories
+# +++ Functions +++
 # not blank function
 def not_blank(question):
     valid = False
@@ -12,7 +11,22 @@ def not_blank(question):
 
         else:
             print("This can't be blank please enter a name")
+
 #Int checker
+def int_check(question):
+    error = "Please enter a full number between 12 and 130"
+
+    valid = False
+    while not valid:
+
+        try:
+            response = int(input(question))
+            return response
+
+        except ValueError:
+            print(error)
+
+# ******* Main Routine *******
 name = ""
 count = 0
 max_tickets = 150
@@ -22,14 +36,6 @@ while name != "xxx" and count < max_tickets:
 
     # Get details
     name = not_blank("Name: ")
-    count += 1
-    print ()
-
-    if count == max_tickets:
-        print("We have sold out of tickets!")
-else:
-    print("you have sold {} tickets.    \n")
-    "there are {} places still availble" .format(count, max_tickets - count)
 
 #ask the user if they have used the program before or show
 
@@ -37,7 +43,8 @@ else:
     #Get name
 #name = not_blank("Name: ")
     #get age
-age = int_check("Age: ")
+    age = int_check("Age: ")
+
     #check that the age is valid
     if age < 12:
         print("Sorry you are too young for this movie")
@@ -45,6 +52,15 @@ age = int_check("Age: ")
     elif age > 130:
         print("This is very old - it looks like a mistake")
         continue
+
+    count += 1
+    print ()
+
+    if count == max_tickets:
+        print("We have sold out of tickets!")
+    else:
+        print("you have sold {} tickets.    \n")
+        "there are {} places still availble" .format(count, max_tickets - count)
     #calculate ticket price
 
     #loop to ask for snacks
