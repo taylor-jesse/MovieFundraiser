54b2077ec0e2d65537cd8d9996369ad104f973a6
taylor.jesse <taylor.jesse@awatapu.school.nz>
2021-08-05 14:37:37 +1200

    Int Checker (working)

--- int_checker.py
+++ int_checker.py
@@ -1,5 +1,6 @@
-error_age = ("Please enter a full number between 12 and 130")
+
 def int_check(question, low_num, high_num):
+    error = ("Please enter a full number between 12 and 130")
 
     valid = False
     while not valid:
@@ -7,12 +8,12 @@ def int_check(question, low_num, high_num):
         try:
             response = int(input(question))
 
-        if low_num < response < high_num:
-           return response
-        else:
-            print (error_age)
+            if low_num <= response <= high_num:
+               return response
+            else:
+                print (error)
 
-except ValueError
-    print(error_age)
+        except ValueError:
+            print(error)
 
 age = int_check("Age: ", 12, 130)
